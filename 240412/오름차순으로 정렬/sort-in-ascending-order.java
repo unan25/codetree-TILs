0d1e950356880739b68d86408810ee7149a29bfb
Unan <137999702+unan25@users.noreply.github.com>
2024-04-12 03:27:07 +0900

    Title: 오름차순으로 정렬, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240412/오름차순으로 정렬/sort-in-ascending-order.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i : arr){
+            sb.append(i).append(" ");
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
