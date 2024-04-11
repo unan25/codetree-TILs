294c4b19fec526c1b9a9f1ef345e6d463e773a75
Unan <137999702+unan25@users.noreply.github.com>
2024-04-12 02:52:00 +0900

    Title: 격자 반대로 채우기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240412/격자 반대로 채우기/grid-anti-fill.java
+++ 240412/격자 반대로 채우기/grid-anti-fill.java
@@ -12,14 +12,29 @@ public class Main {
         int[][] arr = new int[n][n];
 
         int num = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (i % 2 == 0) {
-                for (int j = 0; j < n; j++) {
-                    arr[j][i] = num++;
+
+        if (n % 2 == 0) {
+            for (int i = n - 1; i >= 0; i--) {
+                if (i % 2 == 0) {
+                    for (int j = 0; j < n; j++) {
+                        arr[j][i] = num++;
+                    }
+                } else {
+                    for (int j = n - 1; j >= 0; j--) {
+                        arr[j][i] = num++;
+                    }
                 }
-            } else {
-                for (int j = n - 1; j >= 0; j--) {
-                    arr[j][i] = num++;
+            }
+        } else {
+            for (int i = n - 1; i >= 0; i--) {
+                if (i % 2 != 0) {
+                    for (int j = 0; j < n; j++) {
+                        arr[j][i] = num++;
+                    }
+                } else {
+                    for (int j = n - 1; j >= 0; j--) {
+                        arr[j][i] = num++;
+                    }
                 }
             }
         }
@@ -31,6 +46,7 @@ public class Main {
             }
             sb.append("\n");
         }
+
 
         bw.write(sb.toString().trim());
 
