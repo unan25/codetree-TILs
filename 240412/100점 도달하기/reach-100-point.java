711685bca4370feafc4fc945249de7e30a6551b5
Unan <137999702+unan25@users.noreply.github.com>
2024-04-12 00:22:05 +0900

    Title: 100점 도달하기, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240412/100점 도달하기/reach-100-point.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i <= 100; i++) {
+            if (i >= 90) sb.append("A").append(" ");
+            else if (i >= 80) sb.append("B").append(" ");
+            else if (i >= 70) sb.append("C").append(" ");
+            else if (i >= 60) sb.append("D").append(" ");
+            else sb.append("E").append(" ");
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
