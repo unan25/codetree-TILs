65d3d033bb32a4631392a542c8407c587b2bb116
Unan <137999702+unan25@users.noreply.github.com>
2024-04-14 20:49:00 +0900

    Title: 문자열의 총 길이 구하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240414/문자열의 총 길이 구하기/find-the-length-of-the-string.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+        while (st.hasMoreTokens()) {
+            sum += Integer.parseInt(String.valueOf(st.nextToken().length()));
+        }
+
+        bw.write(String.valueOf(sum));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
