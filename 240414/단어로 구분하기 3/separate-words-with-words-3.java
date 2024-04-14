a380da334222c1f618c7e46c66b834a1982199a4
Unan <137999702+unan25@users.noreply.github.com>
2024-04-14 20:58:10 +0900

    Title: 단어로 구분하기 3, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240414/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] arr = new String[st.countTokens()];
+
+        int num = 0;
+
+        while (st.hasMoreTokens()) {
+            arr[num++] = st.nextToken();
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]).append("\n");
+        }
+
+        bw.write(sb.toString().trim());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
